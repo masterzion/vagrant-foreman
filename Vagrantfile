838c5a220e858d6503294a9f3fcaea29bb94806d
Jairo Moreno <master.zion@gmail.com>
2017-09-24 23:58:00 +0200

    first commit

--- Vagrantfile
+++ Vagrantfile
@@ -2,27 +2,17 @@
 # vi: set ft=ruby :
 
 Vagrant.configure(2) do |config|
-
-#  This tells vagrant which box virtualbox should use
-
-  config.vm.box = "ubuntu/trusty64"
-
-#  This enables port forwarding to access the foreman instance
-
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network "forwarded_port", guest: 443, host: 8080
 
-  config.vm.network "public_network", bridge: "eth2", ip: "192.168.57.10"
-
-#  If you need to manage more network, just add more bridge if's
-#  config.vm.network "public_network", bridge: "eth4", ip: "192.168.56.10"
+#  config.vm.network "public_network", bridge: "eth2", ip: "192.168.57.10"
 
   config.vm.synced_folder ".", "/vagrant", disabled: true
-  
   config.vm.hostname = "domain.localhost"
 
   config.vm.provider "virtualbox" do |vb|
    vb.gui = false
-   vb.name = "Foreman-110"
+   vb.name = "Foreman-150"
    vb.memory = "2048"
   end
 
--- files/script.sh
+++ files/script.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-    echo 127.0.0.1 localhost > /etc/hosts
-    echo 192.168.57.10 domain.domain.localhost foreman >> /etc/hosts
 
-    locale-gen de_CH.UTF-8
-    echo LANG="de_CH.UTF-8" > /etc/default/locale
+apt-get update
+apt-get upgrade -y
 
-    apt-get update
-    apt-get upgrade -y
+sudo apt-get install language-pack-pt
 
-    apt-get -y install ca-certificates
-    wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-    dpkg -i puppetlabs-release-trusty.deb
+#echo "127.0.0.1  foreman.localdomain foreman   localhost" >> /etc/hosts
+#echo "192.168.57.10   foreman.localdomain foreman  domain.domain.localhost foreman" >> /etc/hosts
 
-    echo "deb http://deb.theforeman.org/ trusty 1.10" > /etc/apt/sources.list.d/foreman.list
-    echo "deb http://deb.theforeman.org/ plugins 1.10" >> /etc/apt/sources.list.d/foreman.list
-    wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 
-    apt-get update && apt-get -y install foreman-installer
+apt-get -y install ca-certificates
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
 
-    reboot
+echo "deb http://deb.theforeman.org/ xenial 1.15" > /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 1.15" >> /etc/apt/sources.list.d/foreman.list
+apt-get -y install ca-certificates
+wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+
+apt-get install language-pack-pt
+
+apt-get update && apt-get -y install foreman-installer
+
+/usr/sbin/foreman-installer > /home/ubuntu/foreman.log
+
+reboot
 
 exit 0;
